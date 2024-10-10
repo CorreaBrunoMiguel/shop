e5f62873de51575383573e44f88c61886375997e
CorreaBrunoMiguel <bruno_miguel-correa@etudante.sesisenai.org.br>
2024-10-10 10:31:28 -0300

    Criado as entities (Product, Category, Image) e suas relaçoes.

---
+++ src/main/java/org/correa/dreamshops/model/Image.java
@@ -0,0 +1,33 @@
+package org.correa.dreamshops.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Blob;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "images")
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private String fileName;
+    private String fileType;
+    @Lob
+    private Blob image;
+    private String downloadUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
